bce04baf19aec747e98eacf50952bb6a5813e14f
Karine <karine.fbueno@gmail.com>
2024-05-15 17:24:20 -0300

    feat: requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,18 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+        return
+
+    try:
+        with open(path_file, "r", encoding="utf-8") as file:
+            lines = file.read().splitlines()
+        return lines
+    except FileNotFoundError:
+        print(
+            f"""Arquivo {path_file} não encontrado""",
+            file=sys.stderr,
+        )
+        return
