c061a9863b21da3127d77fd4a9c2d91eb4e1297b
Karine <karine.fbueno@gmail.com>
2024-05-15 18:03:55 -0300

    feat: requisito 3 e 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,35 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+import sys
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def process(path_file, instance: Queue):
+    for item in instance.queue:
+        if item["nome_do_arquivo"] == path_file:
+            return
+
+    linhas = txt_importer(path_file)
+
+    dicionario = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(linhas),
+        "linhas_do_arquivo": linhas,
+    }
+
+    instance.enqueue(dicionario)
+    print(dicionario)
+
+
+def remove(instance: Queue):
+    if len(instance.queue) == 0:
+        return print("Não há elementos", file=sys.stdout)
+    else:
+        removed_file = instance.dequeue()
+        return print(
+            f"""Arquivo {removed_file['nome_do_arquivo']}
+              removido com sucesso""",
+            file=sys.stdout,
+        )
 
 
 def file_metadata(instance, position):
